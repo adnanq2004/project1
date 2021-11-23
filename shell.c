ec3a528350af122a50753585152f98c740d888bc
adnanq2004 <aquayyum20@stuy.edu>
2021-11-22 22:04:41 -0500

    it can make a shell that can exitgit add shell.c

--- shell.c
+++ shell.c
@@ -1,3 +1,18 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int argcount(char *line){
+  int i;
+  int c = 1;
+  for (i = 0; i < strlen(line); i ++){
+    if (line[i] == ' ')
+      c ++;
+  }
+  return c;
+}
+
 char **parse_args( char *line ) {
     char *temp;
     int counter;
@@ -10,19 +25,55 @@ char **parse_args( char *line ) {
     return returnline;
 }
 
+/*
+static void sighandler(int sig) {
+	if (sig == SIGINT) {
+		exit(0);
+	}
+}
+*/
+
 int main() {
 
 	int ex = 0;
-	int waiting = 1;
-	while (!ex) {
-	
+	//int waiting = 0;
+	int child = 0;
+	int i = 0;
+	while (!ex && i < 10) {
+		i++;
+		printf("\nSALT:~$ ");
+		char data[256];
+		fgets(data, sizeof(data), stdin);
+		char ** args = parse_args(data);
+		if (!strcmp(args[0], "exit\n")) {
+			printf("\nbacking out\n");
+			exit(0);
+		}
+
+		/*
 		if (!waiting) {
 			printf("\nSALT:~$ ");
-			waiting = 0;
+			waiting = 1;
 		}
 		else {
-			
+			char data[256];
+			fgets(data, sizeof(data), stdin);
+			//int nums = argcount(data);
+			char ** args = parse_args(data);
+			if (!strcmp(args[0], "cd")) {
+				printf("supposed to backout");
+			}
+			if (!strcmp(args[0], "exit")) {
+				printf("supposed to kill");
+				execvp("kill", args);
+			}
+			child = fork();
+			execvp(args[0], args);
+			waiting = 0;
 		}
+		*/
+
+		//i++;
 	
 	}
 
